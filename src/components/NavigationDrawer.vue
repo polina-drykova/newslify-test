79b598b8487382316e9cf53c81943541d43325b2
Polina Drykova <polina.drykova1988@gmail.com>
2020-07-12 11:41:14 +0700

    added message if there is no history

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -18,6 +18,7 @@
                 <v-list-item
                 :key="source.id"
                 v-for="source in allSources"
+                class="pl-2"
                 link>
                   <v-list-item-content>
                     <v-list-item-title>{{source.name}}</v-list-item-title>
@@ -29,10 +30,14 @@
             <v-expansion-panel>
               <v-expansion-panel-header style="max-height: 20px;">HISTORY</v-expansion-panel-header>
               <v-expansion-panel-content>
+                <v-list-item class="pl-2" v-if="allVisitedPages.length < 1">
+                  <v-list-item-title>No activity yet..</v-list-item-title>
+                </v-list-item>
                 <v-list-item
                 :key="visitedPage.id"
                 v-for="visitedPage in allVisitedPages"
                 link
+                class="pl-2"
                 :to="{
                   name: 'Details',
                   params: {
