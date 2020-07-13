60a9c61f7ff047cfc8f06a19920f1697a1d439a1
Polina Drykova <polina.drykova1988@gmail.com>
2020-07-13 08:56:11 +0700

    fixed filter logic and rendering issue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <v-app>
-    <NavigationDrawer/>
+    <NavigationDrawer :headlines="allHeadlines" @clicked="onClickChild"/>
     <v-main>
       <!-- Render view: -->
-      <router-view :headlines="allHeadlines" :key="$route.path"></router-view>
-      <!-- <Headlines/> -->
+      <router-view
+        :headlines="filteredHeadlines"
+        :filterOpt="filterOpt"
+        :key="$route.path">
+      </router-view>
     </v-main>
   </v-app>
 </template>
@@ -15,16 +18,34 @@ import NavigationDrawer from './components/NavigationDrawer.vue';
 
 export default {
   name: 'App',
-
   components: {
     NavigationDrawer,
-    // Headlines,
+  },
+  data() {
+    return {
+      filterOpt: '', // Set to the value emitted by the child (NavigationDrawer)
+    };
   },
   methods: {
     ...mapActions(['fetchHeadlines']),
+    // Get the filter option from nav drawer and assign it to data:
+    onClickChild(value) {
+      this.filterOpt = value;
+      // Check if it's details page and redirect if true:
+      if (this.$route.name === 'Details') {
+        this.$router.push('/');
+      }
+    },
   },
   computed: {
     ...mapGetters(['allHeadlines', 'getHeadlineById']),
+    // modify headlines if there's filter:
+    filteredHeadlines() {
+      if (this.filterOpt !== '' && this.filterOpt !== 'TOP-20') {
+        return this.allHeadlines.filter((item) => item.source.id === this.filterOpt);
+      }
+      return this.allHeadlines;
+    },
   },
   created() {
     // import headlines:
